c97377ef29bfbf9f90ea6cccd9bf626d69f41a6c
smithrm941 <smithrm941@gmail.com>
2017-09-25 15:24:50 -0700

    front end for signup page

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -9,4 +9,18 @@ router.get('/login', (request, response) => {
   response.render('login')
 })
 
+router.post('/login', (request, response) => {
+  const {email, password} = request.body;
+  console.log(email, password)
+})
+
+router.get('/signup', (request, response) => {
+  response.render('signup')
+})
+
+router.post('/signup', (request, response) => {
+  const {email, password} = request.body;
+  console.log(email, password)
+})
+
 module.exports = router
